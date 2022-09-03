4875c4938889b40d5ed97d71fe1277f79d8db1cc
zhman1981 <zhman@ukr.net>
2022-09-03 11:48:32 +0300

    finish

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { css } from '@emotion/css';
 
-const Contacts = ({ contacts }) => {
+const Contacts = ({ contacts, onDeleteContact }) => {
   return (
     <ul>
-      {contacts.map(({ id, name }) => (
+      {contacts.map(({ id, name, number }) => (
         <li key={id}>
-          <p>{name}</p>
+          <p>
+            {name}: {number}
+            <button
+              type="button"
+              onClick={() => onDeleteContact(id)}
+              className={css`
+                margin-left: 20px;
+                border-radius: 4px;
+              `}
+            >
+              Delete
+            </button>
+          </p>
         </li>
       ))}
     </ul>
--- src/components/SubmitForm/SubmitForm.jsx
+++ src/components/SubmitForm/SubmitForm.jsx
@@ -1,47 +1,67 @@
 import React, { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-// import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { css } from '@emotion/css';
+
+const nameRegex = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/g;
+const numberRegex =
+  /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/g;
+
+const ContactSchema = Yup.object().shape({
+  name: Yup.string()
+    .matches(nameRegex, 'Only alphabets are allowed for this field ')
+    .required('Required'),
+  number: Yup.string()
+    .matches(
+      numberRegex,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required('Required'),
+});
 
 export class SubmitForm extends Component {
   state = {
     name: '',
+    number: '',
   };
 
   render() {
     return (
       <div>
-        <p>Name:</p>
         <Formik
-          initialValues={{ name: this.state.name }}
-          validate={values => {
-            const errors = {};
-            if (!values.name) {
-              errors.name = 'Required';
-            } else if (
-              !/^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/i.test(
-                values.name
-              )
-            ) {
-              errors.name =
-                'Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore dArtagnan';
-            }
-            return errors;
-          }}
+          initialValues={{ name: this.state.name, number: this.state.number }}
+          validationSchema={ContactSchema}
           onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              // alert(JSON.stringify(values, null, 2));
-              this.props.onSend(values);
-              this.setState({ name: '' });
-              setSubmitting(false);
-            }, 400);
+            this.props.onSend(values);
+            document.querySelector('Form').reset();
+            setSubmitting(false);
           }}
         >
           {({ isSubmitting }) => (
-            <Form>
+            <Form
+              className={css`
+                display: flex;
+                flex-direction: column;
+                padding: 15px;
+                font-size: 16px;
+                width: 25%;
+                border: 1px solid;
+              `}
+            >
+              <p>Name:</p>
               <Field type="name" name="name" />
               <ErrorMessage name="name" component="div" />
-              <button type="submit" disabled={isSubmitting}>
-                Submit
+              <p>Number:</p>
+              <Field type="name" name="number" />
+              <ErrorMessage name="number" component="div" />
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className={css`
+                  margin-top: 15px;
+                `}
+              >
+                Add contact
               </button>
             </Form>
           )}
@@ -51,7 +71,4 @@ export class SubmitForm extends Component {
   }
 }
 
-// SubmitForm.propTypes = {
-//   name: PropTypes.string,
-// }
 export default SubmitForm;
